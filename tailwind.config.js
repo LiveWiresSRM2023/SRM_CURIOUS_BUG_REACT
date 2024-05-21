7054df224c2f3ca1787594ccbc7af3b018d55163
abin0001 <abinj2003@gamail.com>
2024-05-21 17:26:28 +0530

    Feed Page Finised

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
   ],
 
   theme: {
     extend: {},
   },
   plugins: [
-    require('tailwind-scrollbar-hide')
+    require('tailwind-scrollbar-hide'),
+    require("tw-elements-react/dist/plugin.cjs")
   ]
 }
