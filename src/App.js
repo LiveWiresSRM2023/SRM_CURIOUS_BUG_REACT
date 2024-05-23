bd960b49ae0db72aa60f43708299367abb9bfaf1
abin0001 <abinj2003@gamail.com>
2024-05-23 12:54:27 +0530

    Pulling Profile

--- src/App.js
+++ src/App.js
@@ -14,14 +14,11 @@ const App = () => {
           <Route path='/login' element={<Login />} />
           <Route path='/landing' element={<Landing />} />
           <Route path='/signin' element={<Signin />} />
-           <Route path='/Home' element={<Home/>} />
-
-
+          <Route path='/Home' element={<Home/>} />
         </Routes>
         {/* <Footer /> */}
       </BrowserRouter>
     </div>
   )
 }
-
 export default App;
