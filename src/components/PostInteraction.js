82574ee61bfe9b3315875858f0b9366f39d58d10
imisgsc <simigracia@gmail.com>
2024-07-03 20:30:00 +0530

    adding loading effect page

--- src/components/PostInteraction.js
+++ src/components/PostInteraction.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Profile from './Profilepage'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
 
 const PostInteraction = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); 
+    }, 500);
+  }, []); 
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex space-x-1">
+          <div className="w-3 h-3 bg-yellow-900 rounded-full"></div>
+          <div className="w-3 h-3 bg-yellow-900 rounded-full"></div>
+          <div className="w-3 h-3 bg-yellow-900 rounded-full"></div>
+        </div>
+        <p className="ml-2 text-yellow-900">Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-        <Navbar />
-        
-
-
+      <Navbar />
+      
     </div>
-  )
-}
+  );
+};
 
-export default PostInteraction
+export default PostInteraction;
